58d5fcee48a3f43bf36a938682cd3ca7186365c4
jt1134 <jt1134@gmail.com>
2012-08-07 20:20:32 -0500

    liblight: update for jellybean
    Change-Id: I0ad41eb2a7a5e9062914e936b37c9c8aea239ce6

--- liblight/lights.c
+++ liblight/lights.c
@@ -39,7 +39,7 @@ static int write_int(char const *path, int value)
 	int fd;
 	static int already_warned = 0;
 
-	LOGV("write_int: path %s, value %d", path, value);
+	ALOGV("write_int: path %s, value %d", path, value);
 	fd = open(path, O_RDWR);
 
 	if (fd >= 0) {
@@ -50,7 +50,7 @@ static int write_int(char const *path, int value)
 		return amt == -1 ? -errno : 0;
 	} else {
 		if (already_warned == 0) {
-			LOGE("write_int failed to open %s\n", path);
+			ALOGE("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -71,7 +71,7 @@ static int set_light_notifications(struct light_device_t *dev,
 	int brightness = !!(state->color & 0x00ffffff);
 	int err;
 
-	LOGV("set_light_notifications: color=%#010x, brightness=%u.", state->color,
+	ALOGV("set_light_notifications: color=%#010x, brightness=%u.", state->color,
 	     brightness);
 
 	pthread_mutex_lock(&g_lock);
@@ -96,7 +96,7 @@ static int set_light_backlight(struct light_device_t *dev,
 
 static int close_lights(struct light_device_t *dev)
 {
-	LOGV("close_light is called");
+	ALOGV("close_light is called");
 	if (dev)
 		free(dev);
 
@@ -109,7 +109,7 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	int (*set_light)(struct light_device_t *dev,
 		struct light_state_t const *state);
 
-	LOGV("open_lights: open with %s", name);
+	ALOGV("open_lights: open with %s", name);
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
@@ -138,7 +138,7 @@ static struct hw_module_methods_t lights_module_methods = {
 	.open =  open_lights,
 };
 
-const struct hw_module_t HAL_MODULE_INFO_SYM = {
+struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.tag = HARDWARE_MODULE_TAG,
 	.version_major = 1,
 	.version_minor = 0,
